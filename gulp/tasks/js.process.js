950c36696d6ac4af60c012babe30872229480004
Denys <denv928@gmail.com>
2017-12-22 00:09:26 +0200

    Зроблено до карти

---
+++ gulp/tasks/js.process.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('js:process', function() {
+    return $.gulp.src('./source/js/*.*')
+      .pipe($.gp.sourcemaps.init())
+      .pipe($.gp.concat('app.js'))
+      .pipe($.gp.sourcemaps.write())
+      .pipe($.gulp.dest($.config.root + '/assets/js'))
+  })
+};
