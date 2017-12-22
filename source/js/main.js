c72aea472b83302855cf317dd086657a054e6a45
Denys <denv928@gmail.com>
2017-12-22 16:52:33 +0200

    Майже все зроблено

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,15 @@
- 
+$(function() {
+  var uluru = { lat: 51.417759, lng: -0.52804 };
+  var mapElement = document.getElementById("map");
+
+  var map = new google.maps.Map(mapElement, {
+    center: uluru,
+    position: uluru,
+    zoom: 11,
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+});
